c06c103b6f8a3ae03d37ea59ecc6368b9132fc1c
Nhat Le <nhatlenlh1412@gmail.com>
2023-03-31 00:01:47 +0700

    extract superclass

---
+++ dealing-with-generalization/extractsuperclass/afterchange/extractsuperclassac.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dealing_with_generalization.extractsuperclass.afterchange
+{
+    class Animal {
+        protected String breed;
+        protected int age;
+
+        public Animal(String breed, int age)
+        {
+            this.breed = breed;
+            this.age = age;
+        }
+
+        public String getBreed()
+        {
+            return breed;
+        }
+
+        public void setBreed(String breed)
+        {
+            this.breed = breed;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+    }
+    class Dog: Animal {
+        public Dog(String breed, int age):base(breed, age) { }
+        
+
+        public void bark()
+        {
+            Console.WriteLine("Woof woof");
+        }
+    }
+    class Cat : Animal
+    {
+        public Cat(String breed, int age):base (breed, age) { }
+        
+
+        public void meow()
+        {
+            Console.WriteLine("Meow meow");
+        }
+    }
+}
---
+++ dealing-with-generalization/extractsuperclass/beforechange/extractsuperclassbc.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dealing_with_generalization.extractsuperclass.beforechange
+{
+    class Cat
+    {
+        private string breed;
+        private int age;
+
+        public Cat(string breed, int age)
+        {
+            this.breed = breed;
+            this.age = age;
+        }
+
+        public string getBreed()
+        {
+            return breed;
+        }
+
+        public void setBreed(string breed)
+        {
+            this.breed = breed;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+
+        public void meow()
+        {
+            Console.WriteLine("Meow meow");
+        }
+    }
+    class Dog
+    {
+        private string breed;
+        private int age;
+
+        public Dog(string breed, int age)
+        {
+            this.breed = breed;
+            this.age = age;
+        }
+
+        public string getBreed()
+        {
+            return breed;
+        }
+
+        public void setBreed(string breed)
+        {
+            this.breed = breed;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+
+        public void bark()
+        {
+            Console.WriteLine("Woof woof");
+        }
+    }
+}
