251e5300785bfe9922b382cb9dc60438b5785943
Nhat Le <nhatlenlh1412@gmail.com>
2023-03-30 23:39:35 +0700

    push dowm field

---
+++ dealing-with-generalization/pushdownfield/afterchange/Pushdownfield.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dealing_with_generalization.pushdownfield.afterchange
+{
+    class Employee
+    {
+        public String name;
+        public int ID;
+        
+    }
+    class Engineer : Employee
+        
+    {
+        public String score;
+        public String getScore()
+        {
+            return score;
+        }
+        public String getEngineerScore()
+        {
+            return score + 5;
+        }
+        public void print()
+        {
+            Console.WriteLine("This  Engineer class and this class use grade properties");
+        }
+    }
+    class Manager : Employee
+    {
+        public void print()
+        {
+            Console.WriteLine("This is Manager class and this class don't use grade properties");
+        }
+    }
+}
---
+++ dealing-with-generalization/pushdownfield/beforechange/pushdownfield.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dealing_with_generalization.pushdownfield.beforechange
+{
+    class Employee
+    {
+        public String name;
+        public int ID;
+        public String score;
+        public String getScore()
+        {
+            return score;
+        }
+        private void setScore(String score)
+        {
+            this.score = score;
+        }
+    }
+    class Engineer: Employee
+    {
+        public String getEngineerScore()
+        {
+            return score + 5;
+        }
+        public void print()
+        {
+            Console.WriteLine("This  Engineer class and this class use grade properties");
+        }
+    }
+    class Manager: Employee
+    {
+        public void print()
+        {
+            Console.WriteLine("This is Manager class and this class don't use grade properties");
+        }
+    }
+}
