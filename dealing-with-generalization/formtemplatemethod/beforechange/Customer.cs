25541d40d337c109ab5162c40e65daeb3c021371
Nhat Le <nhatlenlh1412@gmail.com>
2023-03-31 16:44:36 +0700

    form template method

---
+++ dealing-with-generalization/formtemplatemethod/beforechange/Customer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dealing_with_generalization.formtemplatemethod.beforechange
+{
+    class Customer
+    {
+        private string name;
+
+        private List<Rental> rentals = new ArrayList<>();
+
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+
+        public void addRental(Rental arg)
+        {
+            rentals.Add(arg);
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string statement()
+        {
+            int frequentRenterPoints = 0;
+            string result = "Rental Record for " + getName() + "\n";
+            foreach ( var rentals in Rental)
+            {
+                frequentRenterPoints += each.frequentRenterCount();
+
+                result += "\t" + each.getMovie().getTitle() + "\t" + each.countAmount() + "\n";
+            }
+
+            result += " Amount owed is " + countTotalAmount() + "\n";
+            result += " You earned " + frequentRenterPoints + " frequent renter points";
+            return result;
+        }
+
+        public string htmlStatement()
+        {
+            int frequentRenterPoints = 0;
+            string result = "<H1>Rental Record for <EM> " + getName() + "</EM><H1><P>n";
+            foreach (var rentals in Rental)
+            {
+                frequentRenterPoints += each.frequentRenterCount();
+
+                result += "\t" + each.getMovie().getTitle() + "\t" + each.countAmount() + "<BR>\n";
+            }
+
+            result += "<P> Amount owed is <EM>" + countTotalAmount() + "</EM><P>\n";
+            result += " You earned <EM>" + frequentRenterPoints + " </EM> frequent renter points <P>";
+            return result;
+        }
+
+        private double countTotalAmount()
+        {
+            double totalAmount = 0;
+
+            foreach (var rentals in Rental)
+            {
+                totalAmount += each.countAmount();
+            }
+
+            return totalAmount;
+        }
+    }
+}
---
+++ dealing-with-generalization/formtemplatemethod/beforechange/Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dealing_with_generalization.formtemplatemethod.beforechange
+{
+    class Movie
+    {
+        public static  int CHILDRENS = 2;
+
+        public static  int REGULAR = 0;
+
+        public static  int NEW_RELEASE = 1;
+
+        private string title;
+
+        private int priceCode;
+
+        public Movie(string title, int priceCode)
+        {
+            this.title = title;
+            this.priceCode = priceCode;
+        }
+
+        public string getTitle()
+        {
+            return title;
+        }
+
+        public int getPriceCode()
+        {
+            return priceCode;
+        }
+
+        public void setPriceCode(int priceCode)
+        {
+            this.priceCode = priceCode;
+        }
+    }
+}
